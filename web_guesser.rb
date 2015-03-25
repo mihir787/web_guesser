dcfed62ef16d8857680a09af39a2d0e9eeb398ac
Mihir Parikh <mihir787@gmail.com>
2015-03-24 19:43:20 -0600

    functioning, no extensions done

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,27 @@
 require 'sinatra'
+require 'sinatra/fix_951'
 require 'sinatra/reloader'
 
-number = rand(100)
+NUMBER = rand(100)
 
 get '/' do
-  p params.inspect
-  erb :index, :locals => {:number => number}
+  guess = params['guess'].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:number => NUMBER, :message => message}
+end
+
+def check_guess(guess)
+  if guess == 0
+    ""
+  elsif NUMBER - guess >= 5
+    "Way too low!"
+  elsif NUMBER - guess <= -5
+    "Way too high!"
+  elsif guess < NUMBER
+    "Too low!"
+  elsif guess > NUMBER
+    "Too high!"
+  else
+    "You got it right!\nThe Secret Number is #{NUMBER}"
+  end
 end
